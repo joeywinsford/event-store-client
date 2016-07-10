d55f88b2163b2f57f15c5ab6d0ad01445a683433
Joe Lloyd <josephglloyd@gmail.com>
2016-07-10 19:53:50 +0100

    Assert that the metadata written can be read out again

--- test/eventMetadata.js
+++ test/eventMetadata.js
@@ -9,62 +9,66 @@ var streamId = "event-store-client-test";
 
 describe("Event Metadata", function() {
 
-    describe("JSON metadata", function() {
     var testEventNumber = null;
     var testRunDate = new Date().toISOString();
 
-    before("Writing a test event with metadata", function(done) {
+    describe("JSON metadata", function() {
+        before("Writing a test event with metadata", function(done) {
 
-        var events = [{
-            eventId: EventStoreClient.Connection.createGuid(),
-            eventType: "MetadataTestEvent",
-            data: {
-                comment: "Testing reading and writing event metadata"
-            },
-            metadata: {
-                testRanAt: testRunDate
-            }
-        }];
-
-        var connection = new EventStoreClient.Connection(createOptions(done));
-        connection.writeEvents(streamId, EventStoreClient.ExpectedVersion.Any, false, events, credentials, onCompleted);
-
-        function onCompleted(completed) {
-            assert.equal(completed.result, EventStoreClient.OperationResult.Success,
-                "Expected a result code of Success, not " + EventStoreClient.OperationResult.getName(completed.result) + ": " + completed.message);
-
-            testEventNumber = completed.firstEventNumber;
-
-            connection.close();
-            done();
-        };
-    });
-    describe("Reading metadata from an event", function() {
-        it("should have metadata defined on the event", function(done) {
-
-            var readEvent = null;
-
-            var maxCount = 1;
-            var onEventAppeared = function (event) {
-                readEvent = event;
-            };
+            var events = [{
+                eventId: EventStoreClient.Connection.createGuid(),
+                eventType: "MetadataTestEvent",
+                data: {
+                    comment: "Testing reading and writing event metadata"
+                },
+                metadata: {
+                    testRanAt: testRunDate
+                }
+            }];
 
             var connection = new EventStoreClient.Connection(createOptions(done));
-            connection.readStreamEventsBackward(streamId, testEventNumber, maxCount, false, false, onEventAppeared, credentials, onCompleted);
+            connection.writeEvents(streamId, EventStoreClient.ExpectedVersion.Any, false, events, credentials, onCompleted);
 
             function onCompleted(completed) {
-                assert.equal(completed.result, EventStoreClient.ReadStreamResult.Success,
-                    "Expected a result code of Success, not " + EventStoreClient.ReadStreamResult.getName(completed.result));
+                assert.equal(completed.result, EventStoreClient.OperationResult.Success,
+                    "Expected a result code of Success, not " + EventStoreClient.OperationResult.getName(completed.result) + ": " + completed.message);
 
-                assert.ok(typeof readEvent.metadata !== "undefined", "Expected event to have metadata");
+                testEventNumber = completed.firstEventNumber;
 
-                assert.ok(readEvent.metadata !== null, "Expected metadata fields to have been present on the event");
                 connection.close();
                 done();
             };
         });
+        describe("Reading metadata from an event", function() {
+            it("should have metadata defined on the event", function(done) {
+
+                var readEvent = null;
+
+                var maxCount = 1;
+                var onEventAppeared = function (event) {
+                    readEvent = event;
+                };
+
+                var connection = new EventStoreClient.Connection(createOptions(done));
+                connection.readStreamEventsBackward(streamId, testEventNumber, maxCount, false, false, onEventAppeared, credentials, onCompleted);
+
+                function onCompleted(completed) {
+                    assert.equal(completed.result, EventStoreClient.ReadStreamResult.Success,
+                        "Expected a result code of Success, not " + EventStoreClient.ReadStreamResult.getName(completed.result));
+
+                    assert.ok(typeof readEvent.metadata !== "undefined", "Expected event to have metadata");
+
+                    assert.ok(readEvent.metadata !== null, "Expected metadata fields to have been present on the event");
+
+                    assert.equal(testRunDate, readEvent.metadata.testRanAt,
+                        "Expected metadata field 'testRanAt' to match date " + testRunDate);
+                    
+                    connection.close();
+                    done();
+                };
+            });
+        });
     });
-});
 });
 
 function createOptions(done) {
