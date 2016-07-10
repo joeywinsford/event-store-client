511bd6ea9ae644d0b66fe1922326038485b7c2fb
Joe Lloyd <josephglloyd@gmail.com>
2016-07-10 20:03:56 +0100

    Add failing test for writing and reading binary data

--- test/eventMetadata.js
+++ test/eventMetadata.js
@@ -58,6 +58,59 @@ describe("Event Metadata", function() {
 
                 assert.equal(testRunDate, readEvent.metadata.testRanAt,
                     "Expected metadata field 'testRanAt' to match date " + testRunDate);
+
+                connection.close();
+                done();
+            };
+        });
+    });
+
+    describe("Reading binary metadata from an event", function() {
+        before("Writing a test event with metadata", function(done) {
+            var events = [{
+                eventId: EventStoreClient.Connection.createGuid(),
+                eventType: "MetadataTestEvent",
+                data: {
+                    comment: "Testing reading and writing event metadata"
+                },
+                metadata: {
+                    testRanAt: new Buffer(testRunDate)
+                }
+            }];
+
+            var connection = new EventStoreClient.Connection(createOptions(done));
+            connection.writeEvents(streamId, EventStoreClient.ExpectedVersion.Any, false, events, credentials, onCompleted);
+
+            function onCompleted(completed) {
+                assert.equal(completed.result, EventStoreClient.OperationResult.Success,
+                    "Expected a result code of Success, not " + EventStoreClient.OperationResult.getName(completed.result) + ": " + completed.message);
+
+                testEventNumber = completed.firstEventNumber;
+
+                connection.close();
+                done();
+            };
+        });
+        it("should have metadata defined on the event", function(done) {
+            var readEvent = null;
+            var maxCount = 1;
+            var onEventAppeared = function (event) {
+                readEvent = event;
+            };
+
+            var connection = new EventStoreClient.Connection(createOptions(done));
+            connection.readStreamEventsBackward(streamId, testEventNumber, maxCount, false, false, onEventAppeared, credentials, onCompleted);
+
+            function onCompleted(completed) {
+                assert.equal(completed.result, EventStoreClient.ReadStreamResult.Success,
+                    "Expected a result code of Success, not " + EventStoreClient.ReadStreamResult.getName(completed.result));
+
+                assert.ok(typeof readEvent.metadata !== "undefined", "Expected event to have metadata");
+
+                assert.ok(readEvent.metadata !== null, "Expected metadata fields to have been present on the event");
+
+                assert.equal(testRunDate, readEvent.metadata.testRanAt,
+                    "Expected metadata field 'testRanAt' to match date " + testRunDate);
                 
                 connection.close();
                 done();
